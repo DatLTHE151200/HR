4613567462cd0aca1da6c1ecd647f51fe2f55468
DatLTHE151200 <97074281+DatLTHE151200@users.noreply.github.com>
2022-03-14 21:04:07 +0700

    Update login features

---
+++ src/java/DAL/DBConnection.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Admin
+ */
+public class DBConnection {
+    public static Connection open() {
+        try {
+            //1. Dang ki driver
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            //2. Tao va mo ket noi
+            return DriverManager.getConnection("jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=QuanLyNhanSu", "sa", "1234567");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+    public static void close(Connection conn, Statement stmt, ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+            if (stmt != null) {
+                stmt.close();
+            }
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String args[]) {
+        try {
+            // connnect to database 'testdb'
+            Connection conn = DriverManager.getConnection("jdbc:sqlserver://localhost\\MSSQLSERVER:1433;databaseName=QuanLyNhanSu", "sa", "1234567");
+            // crate statement
+            Statement stmt = conn.createStatement();
+            // get data from table 'student'
+            ResultSet rs = stmt.executeQuery("select * from NhanViens");
+            // show data
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  " + rs.getString(2) 
+                        + "  " + rs.getString(3));
+            }
+            // close connection
+            conn.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
